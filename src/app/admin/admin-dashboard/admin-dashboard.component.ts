18035f48d47cacf74b44219a08757ce2083b27f8
folatkom <folatko.monika@gmail.com>
2022-05-09 18:52:51 +0200

    add order summary by flavours for admin

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { AuthService } from 'src/app/shared/services/auth.service';
-import {
-  addDoc,
-  Firestore,
-  collection,
-  getDocs,
-} from '@angular/fire/firestore';
-import { TOUCH_BUFFER_MS } from '@angular/cdk/a11y/input-modality/input-modality-detector';
+import { ChosenFlavour } from 'src/app/customer/customer.component';
+import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
@@ -16,12 +11,16 @@ import { TOUCH_BUFFER_MS } from '@angular/cdk/a11y/input-modality/input-modality
 export class AdminDashboardComponent implements OnInit {
   public orders: any = [];
   public ordersByCustomer: any;
+  public allOrders: ChosenFlavour[] = [];
+  public allOrdersSummary: ChosenFlavour[] = [];
+  public isModalVisible = false;
+  public isSummaryVisible = false;
   constructor(private firestore: Firestore) {}
 
   ngOnInit(): void {
-    this.getCustomers();
+    this.getOrdersByCustomers();
   }
-  getCustomers() {
+  getOrdersByCustomers() {
     const dbInstance = collection(this.firestore, 'orders');
     getDocs(dbInstance).then((response) => {
       this.orders = [
@@ -29,9 +28,58 @@ export class AdminDashboardComponent implements OnInit {
           return { ...item.data(), id: item.id };
         }),
       ];
+      this.getAllOrders();
     });
   }
-  getThem() {
-    console.log(this.orders);
+  getAllOrders() {
+    this.allOrders = this.orders.flatMap((item: any) => {
+      return item.order;
+    });
+    this.allOrders.forEach((flavourToCheck) => {
+      if (
+        !this.allOrdersSummary.some(
+          (item: any) => item.name === flavourToCheck.name
+        )
+      ) {
+        this.allOrdersSummary.push(flavourToCheck);
+      } else {
+        let elementToUpdate = this.allOrders.find(
+          (el) => el.name == flavourToCheck.name
+        );
+        flavourToCheck.containers.forEach((containerToCheck) => {
+          if (
+            !elementToUpdate?.containers.some(
+              (item) => item.name === containerToCheck.name
+            )
+          ) {
+            elementToUpdate?.containers.push(containerToCheck);
+          } else {
+            let containerToUpdate = elementToUpdate.containers.find(
+              (el) => el.name == containerToCheck.name
+            );
+            elementToUpdate.containers = elementToUpdate.containers.filter(
+              (item) => item.name !== containerToCheck.name
+            );
+            containerToUpdate!.quantity += containerToCheck.quantity;
+            elementToUpdate.containers.push(containerToUpdate!);
+          }
+        });
+        this.allOrdersSummary = this.allOrdersSummary.filter(
+          (item) => item.name !== flavourToCheck.name
+        );
+        this.allOrdersSummary.push(elementToUpdate!);
+      }
+    });
+  }
+  showModal(value: string) {
+    if (value === 'summary') {
+      this.isSummaryVisible = true;
+    } else {
+      this.isSummaryVisible = false;
+    }
+    this.toggleModal();
+  }
+  toggleModal() {
+    this.isModalVisible = !this.isModalVisible;
   }
 }
