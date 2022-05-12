277d37be14947e890b81e76fae6473d4226d2d3a
folatkom <folatko.monika@gmail.com>
2022-05-12 08:07:41 +0200

    fix order summary

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -6,6 +6,7 @@ import {
   OnDestroy,
 } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Flavour } from 'src/app/shared/model/flavour';
 import { Order } from 'src/app/shared/model/order';
 import { ApiService } from 'src/app/shared/services/api.service';
 
@@ -20,6 +21,8 @@ export class AdminDashboardComponent implements OnInit, OnDestroy {
   public currentOrders: Order[] = [];
   public isModalVisible = false;
   public isSummaryVisible = false;
+  public allOrders: Flavour[] = [];
+  public allOrdersSummary: Flavour[] = [];
   public date = formatDate(new Date(), 'dd/MM/yyyy', 'en-US');
 
   private subscriptions = new Subscription();
@@ -43,10 +46,53 @@ export class AdminDashboardComponent implements OnInit, OnDestroy {
           this.currentOrders.push(el);
         }
       });
+      console.log(this.currentOrders);
+      this.getAllOrders();
     });
     this.subscriptions.add(sub);
   }
-
+  getAllOrders() {
+    this.allOrders = this.currentOrders.flatMap((item: any) => {
+      return item.order;
+    });
+    this.allOrders.forEach((flavourToCheck) => {
+      if (
+        !this.allOrdersSummary.some(
+          (item: Flavour) => item.name === flavourToCheck.name
+        )
+      ) {
+        this.allOrdersSummary.push(flavourToCheck);
+      } else {
+        let elementToUpdate = this.allOrders.find(
+          (el) => el.name == flavourToCheck.name
+        );
+        flavourToCheck.containers.forEach((containerToCheck) => {
+          if (
+            !elementToUpdate?.containers.some(
+              (item) => item.name === containerToCheck.name
+            )
+          ) {
+            console.log(elementToUpdate);
+            elementToUpdate?.containers.push(containerToCheck);
+          } else {
+            let containerToUpdate = elementToUpdate.containers.find(
+              (el) => el.name == containerToCheck.name
+            );
+            elementToUpdate.containers = elementToUpdate.containers.filter(
+              (item) => item.name !== containerToCheck.name
+            );
+            containerToUpdate!.quantity += containerToCheck.quantity;
+            elementToUpdate.containers.push(containerToUpdate!);
+          }
+        });
+        this.allOrdersSummary = this.allOrdersSummary.filter(
+          (item) => item.name !== flavourToCheck.name
+        );
+        this.allOrdersSummary.push(elementToUpdate!);
+      }
+    });
+    console.log(this.allOrdersSummary);
+  }
   showModal(value: string) {
     if (value === 'summary') {
       this.isSummaryVisible = true;
